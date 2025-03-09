e7b2007194fd44b55aa480769017993e388c481f
muhammadbalawal <2230641@johnabbottcollege.net>
2025-03-09 12:05:24 +0000

    added lib

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,12 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@lib": path.resolve(__dirname, "./src/lib"), // Add this
     },
   },
 })
